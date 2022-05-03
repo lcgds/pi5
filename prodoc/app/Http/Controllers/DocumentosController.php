98238206b7008dc9564eb49c3a2fbdcab15dc393
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2022-05-02 21:42:16 -0300

    Comentários sobre funções
    Comentários sobre funções

--- prodoc/app/Http/Controllers/DocumentosController.php
+++ prodoc/app/Http/Controllers/DocumentosController.php
@@ -6,13 +6,15 @@ use Illuminate\Http\Request;
 
 class DocumentosController extends Controller
 {
+    //Pagina Inicial
     public function index(){
         return view('documento.index');
     }
-
+    //Criar Documento
     public function create(){
         return view('documento.create');
         }
+    //Armazenar dados no banco
         public function store(Request $request){
           dd ($request->all());
             }

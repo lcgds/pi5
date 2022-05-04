53d378a00d89f3e2190e3cddb6d703220ed561d8
Luiz Carlos <63129289+lcgds@users.noreply.github.com>
2022-05-03 23:30:09 -0300

    Add header and footer components

---
+++ prodoc/resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="br-footer pt-3" id="footer">
+    <div class="container-lg">
+        <div class="info">
+            <div class="text-down-01 text-medium pb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</div>
+        </div>
+    </div>
+</footer>

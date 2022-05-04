f42ec295f65e68037af8c859688ae83915063289
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2022-05-04 11:00:05 -0300

    Pagina inicial Funcionarios

---
+++ prodoc/resources/views/funcionario/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    
+    <title>Lista de Funcionarios</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+    <h1>Lista de Funcionarios</h1>
+    <a href="{{Route('funcionario.create') }}" class="btn btn-lg btn-primary">Criar Funcionario</a>
+</body>
+</html>

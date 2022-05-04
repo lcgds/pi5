0ed6de62ba4870d2b47a512e6a3fbfe9c0c3a0a8
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2022-05-04 10:52:48 -0300

    Controller de Funcionário
    Controller de Funcionário

---
+++ prodoc/app/Http/Controllers/FuncionarioController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Funcionario;
+
+class FuncionarioController extends Controller
+{
+      //Pagina Inicial
+      public function index(){
+        return view('funcionario.index');
+    }
+    //Criar Documento
+    public function create(){
+        return view('funcionario.create');
+        }
+    //Armazenar dados no banco
+        public function store(Request $request){
+          funcionario::create($request->all());
+            }
+}

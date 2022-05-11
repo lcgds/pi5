f142c69196ed41d65250da0972f0a81f8b5d8729
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2022-05-10 21:23:35 -0300

    Integração com o banco - Clientes

--- prodoc/resources/views/cliente/index.blade.php
+++ prodoc/resources/views/cliente/index.blade.php
@@ -11,5 +11,40 @@ integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jI
 <body>
     <h1>Lista de Clientes</h1>
     <a href="{{Route('cliente.create') }}" class="btn btn-lg btn-primary">Criar Clientes</a>
+<div class="row">
+    <table class="tabela table-striped">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>CNPJ</th>
+                <th>Email</th>
+                <th>Telefone</th>
+            </tr>
+    <thead>
+        <tbody>
+            <!--Pega as informações do banco-->
+            @foreach($clientes as $cli)
+            <tr>
+                    <td>{{ $cli->id }}</td>
+                    <td>{{$cli->nome}}a</td>
+                    <td>{{$cli->cnpj}}</td>
+                    <td>{{$cli->email}}</td>
+                    <td>{{$cli->telefone}}</td>
+                    <td> 
+                        <a hred="#" class="btn btn-sm btn-info">Visualizar</a>
+                        <a hred="#" class="btn btn-sm btn-warning">Editar</a>
+                        <a hred="#" class="btn btn-sm btn-danger">Apagar</a>
+                        </td>
+            </tr>
+            @endforeach
+                    </tbody>
+
+</table>
+
+</div>
+
+
+
 </body>
 </html>

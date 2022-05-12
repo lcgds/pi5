facbd49a92f4ecb256d2b1b1882ba0b7df334271
Luiz Carlos <63129289+lcgds@users.noreply.github.com>
2022-05-11 22:14:29 -0300

    Front-end update

--- prodoc/resources/views/funcionario/create.blade.php
+++ prodoc/resources/views/funcionario/create.blade.php
@@ -1,34 +1,125 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <title>Lista de Funcionarios</title>
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body class = "container mt-5">
-    <h1>Cria Funcionarios</h1>
-    <form method="POST" action = "{{ route('funcionario.store') }}">
-        @csrf
-        <div class="row">
-            <span class="form-label">Id funcionario</span>
-            <input type="text" name="id" class="form-control">
-        </div>
-        <div class="row">
-            <span class="form-label">Nome</span>
-            <textarea class="form-control" name="nome"></textarea>
-        </div>
-        <div class="row">
-            <span class="form-label">Email</span>
-            <textarea class="form-control" name="email"></textarea>
-        </div>
-        <div class="row">
-            <span class="form-label">Telefone</span>
-            <textarea class="form-control" name="telefone"></textarea>
-        </div>
-        <div class="row mt-4">
-    <button type="submit" class="btn btn-sucess btn-lg">Salvar</button>
-</div>
-</form>
+
+<x-head />
+
+<body>
+    <x-header />
+
+    <main class="container">
+
+        <h1 class="my-5">Cadastro de Funcionário</h1>
+
+        <form class="bg-gray-3 p-5 mb-5" method="POST" action="{{ route('funcionario.store') }}">
+            @csrf
+
+            <p class="text-bold">Informações básicas</p>
+
+            <!--<div class="row">
+                <span class="form-label">Id funcionario</span>
+                <input type="text" name="id" class="form-control">
+            </div>-->
+
+            <div class="row">
+
+                <div class="col-lg-9 col-md-8 mb-4">
+                    <div class="br-input">
+                        <label for="input-cliente-nome" class="form-label">Nome</label>
+                        <div class="input-group">
+                            <div class="input-icon"><i class="fas fa-user" aria-hidden="true"></i>
+                            </div>
+                            <input id="input-cliente-nome" type="text" class="form-control" name="nome"
+                                placeholder="Ex.: Fulano"></input>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 mb-4">
+
+                    <div class="br-select">
+                        <div class="br-input">
+                            <label for="select-departamento">Departamento</label>
+                            
+                            <div class="input-group">
+                                <div class="input-icon">
+                                <i class="fas fa-briefcase"></i>
+                                </div>
+                                <input id="select-departamento" type="text" placeholder="Selecione um departamento" />
+                            </div>
+                            
+                            <button class="br-button circle small" type="button" tabindex="-1" data-trigger>
+                                <span class="sr-only">Exibir lista</span><i class="fas fa-angle-down"></i>
+                            </button>
+                        </div>
+
+                        <div class="br-list" tabindex="0">
+
+                            <div class="br-item divider" tabindex="-1">
+                                <div class="br-radio">
+                                    <input id="rb0" type="radio" name="opcao" value="opcao1" />
+                                    <label for="rb0">Opção 1</label>
+                                </div>
+                            </div>
+
+                            <div class="br-item divider" tabindex="-1">
+                                <div class="br-radio">
+                                    <input id="rb1" type="radio" name="opcao" value="opcao2" />
+                                    <label for="rb1">Opção 2</label>
+                                </div>
+                            </div>
+
+                            <div class="br-item divider" tabindex="-1">
+                                <div class="br-radio">
+                                    <input id="rb2" type="radio" name="opcao" value="opcao3" />
+                                    <label for="rb2">Opção 3</label>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+
+            <p class="text-bold">Dados para contato</p>
+
+            <div class="row">
+                <div class="col-lg-8 col-md-8 mb-4">
+                    <div class="br-input">
+                        <label for="input-cliente-email" class="form-label">E-mail</label>
+                        <div class="input-group">
+                            <div class="input-icon"><i class="fas fa-envelope" aria-hidden="true"></i>
+                            </div>
+                            <input id="input-cliente-email" type="email" class="form-control" name="email"
+                                placeholder="Ex.: fulano@email.com.br"></input>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-4 mb-4">
+                    <div class="br-input">
+                        <label for="input-cliente-telefone" class="form-label">Telefone</label>
+                        <div class="input-group">
+                            <div class="input-icon"><i class="fas fa-phone" aria-hidden="true"></i>
+                            </div>
+                            <input type="tel" id="input-cliente-telefone" class="form-control" name="telefone"
+                                placeholder="Ex.: 99999-9999"></input>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="d-flex justify-content-end">
+                <button class="br-button secondary mr-4" type="button">Cancelar</button>
+                <button type="submit" class="br-button primary">Cadastrar</button>
+            </div>
+
+        </form>
+
+    </main>
 </body>
+
 </html>
--- prodoc/resources/views/tipoDocumento/create.blade.php
+++ prodoc/resources/views/tipoDocumento/create.blade.php
@@ -1,31 +1,37 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <title>Lista de Tipos Documentos</title>
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body class = "container mt-5">
-    <h1>Cria Tipos Documentos</h1>
-    <form method="POST" action = "{{ route('tipodocumento.store') }}">
-        @csrf
-        <div class="row">
-            <span class="form-label">Id</span>
-            <input type="text" name="id" class="form-control">
-        </div>
-        <div class="row">
-            <span class="form-label">Nome</span>
-            <textarea class="form-control" name="nome"></textarea>
-        </div>
-        <div class="row">
-            <span class="form-label">Descrição</span>
-            <textarea class="form-control" name="descricao"></textarea>
-        </div>
-       
-        <div class="row mt-4">
-    <button type="submit" class="btn btn-sucess btn-lg">Salvar</button>
-</div>
-</form>
+
+<x-head />
+
+<body>
+    <x-header />
+
+    <main class="container">
+        
+        <h1 class="my-5">Cadastrar Tipo de Documento</h1>
+
+        <form method="POST" action="{{ route('tipodocumento.store') }}">
+            @csrf
+            <div class="row">
+                <span class="form-label">Id</span>
+                <input type="text" name="id" class="form-control">
+            </div>
+            <div class="row">
+                <span class="form-label">Nome</span>
+                <textarea class="form-control" name="nome"></textarea>
+            </div>
+            <div class="row">
+                <span class="form-label">Descrição</span>
+                <textarea class="form-control" name="descricao"></textarea>
+            </div>
+
+            <div class="d-flex justify-content-end">
+                <button class="br-button secondary mr-4" type="button">Cancelar</button>
+                <button type="submit" class="br-button primary">Cadastrar</button>
+            </div>
+        </form>
+    </main>
+
 </body>
+
 </html>

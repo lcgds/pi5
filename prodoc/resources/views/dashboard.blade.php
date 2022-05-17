a6a314c3af75320274a2d431f2518e36793c41d4
Luiz Carlos <63129289+lcgds@users.noreply.github.com>
2022-05-16 21:53:58 -0300

    Front-end update

---
+++ prodoc/resources/views/dashboard.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+
+<html lang="pt-BR">
+
+<x-head/>
+
+<body>
+
+    <x-header/>
+
+    <main class="container">
+        <h2 class="my-5">Dashboard</h2>
+
+        <div class="d-flex flex-sm-wrap justify-content-around align-content-around">
+
+            <div class="br-card py-2 px-4 mb-sm-6 rounder-sm">
+                <a class="text-gray-80" href="http://">
+                    <div class="card-header">
+                        <div class="text-weight-semi-bold text-up-02">Documentos</div>
+                    </div>
+                    <div class="card-content">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="168" viewBox="0 0 24 24" fill="#144fb0">
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M11.17 8l-.58-.59L9.17 6H4v12h16V8h-8z" opacity=".3" />
+                            <path
+                                d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V6h5.17l1.41 1.41.59.59H20v10z" />
+                        </svg>
+                    </div>
+                </a>
+            </div>
+
+            <div class="br-card py-2 px-4 mb-sm-6 rounder-sm">
+                <a class="text-gray-80" href="http://">
+                    <div class="card-header">
+                        <div class="text-weight-semi-bold text-up-02">Clientes</div>
+                    </div>
+
+                    <div class="card-content">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="168" viewBox="0 0 24 24" fill="#144fb0">
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M12 11h2v2h-2v2h2v2h-2v2h8V9h-8v2zm4 0h2v2h-2v-2zm0 4h2v2h-2v-2z" opacity=".3" />
+                            <path
+                                d="M16 15h2v2h-2zm0-4h2v2h-2zm6-4H12V3H2v18h20V7zM6 19H4v-2h2v2zm0-4H4v-2h2v2zm0-4H4V9h2v2zm0-4H4V5h2v2zm4 12H8v-2h2v2zm0-4H8v-2h2v2zm0-4H8V9h2v2zm0-4H8V5h2v2zm10 12h-8v-2h2v-2h-2v-2h2v-2h-2V9h8v10z" />
+                        </svg>
+                    </div>
+                </a>
+            </div>
+
+            <div class="br-card py-2 px-4 mb-sm-6 rounder-sm">
+                <a class="text-gray-80" href="http://">
+                    <div class="card-header">
+                        <div class="text-weight-semi-bold text-up-02">Funcionários</div>
+                    </div>
+
+                    <div class="card-content">
+                        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" width="168"
+                            viewBox="0 0 24 24" fill="#144fb0">
+                            <g>
+                                <rect fill="none" height="24" width="24" y="0" />
+                            </g>
+                            <g>
+                                <g>
+                                    <path
+                                        d="M14,13.5h4V12h-4V13.5z M14,16.5h4V15h-4V16.5z M20,7h-5V4c0-1.1-0.9-2-2-2h-2C9.9,2,9,2.9,9,4v3H4C2.9,7,2,7.9,2,9v11 c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V9C22,7.9,21.1,7,20,7z M11,4h2v5h-2V4z M20,20H4V9h5c0,1.1,0.9,2,2,2h2c1.1,0,2-0.9,2-2h5V20 z M9,15c0.83,0,1.5-0.67,1.5-1.5c0-0.83-0.67-1.5-1.5-1.5s-1.5,0.67-1.5,1.5C7.5,14.33,8.17,15,9,15z M11.08,16.18 C10.44,15.9,9.74,15.75,9,15.75s-1.44,0.15-2.08,0.43C6.36,16.42,6,16.96,6,17.57V18h6v-0.43C12,16.96,11.64,16.42,11.08,16.18z" />
+                                    <path
+                                        d="M13,11h-2c-1.1,0-2-0.9-2-2H4v11h16V9h-5C15,10.1,14.1,11,13,11z M9,12c0.83,0,1.5,0.67,1.5,1.5 c0,0.83-0.67,1.5-1.5,1.5s-1.5-0.67-1.5-1.5C7.5,12.67,8.17,12,9,12z M12,18H6v-0.43c0-0.6,0.36-1.15,0.92-1.39 C7.56,15.9,8.26,15.75,9,15.75s1.44,0.15,2.08,0.43c0.55,0.24,0.92,0.78,0.92,1.39V18z M18,16.5h-4V15h4V16.5z M18,13.5h-4V12h4 V13.5z"
+                                        opacity=".3" />
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                </a>
+            </div>
+
+            <div class="br-card py-2 px-4 mb-sm-6 rounder-sm">
+                <a class="text-gray-80" href="http://">
+                    <div class="card-header">
+                        <div class="text-weight-semi-bold text-up-02">Departamentos</div>
+                    </div>
+                    <div class="card-content">
+                        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" width="168" viewBox="0 0 24 24" fill="#144fb0"><rect fill="none" height="24" width="24"/><path d="M3,10.5c-0.55,0-1-0.45-1-1c0-0.55,0.45-1,1-1s1,0.45,1,1C4,10.05,3.55,10.5,3,10.5z M6,21 c-0.55,0-1-0.45-1-1c0-0.55,0.45-1,1-1s1,0.45,1,1C7,20.55,6.55,21,6,21z M11,3c0-0.55,0.45-1,1-1s1,0.45,1,1c0,0.55-0.45,1-1,1 S11,3.55,11,3z M12,15c-1.38,0-2.5-1.12-2.5-2.5c0-1.38,1.12-2.5,2.5-2.5s2.5,1.12,2.5,2.5C14.5,13.88,13.38,15,12,15z M18,19 c0.55,0,1,0.45,1,1c0,0.55-0.45,1-1,1s-1-0.45-1-1C17,19.45,17.45,19,18,19z M21,10.5c-0.55,0-1-0.45-1-1c0-0.55,0.45-1,1-1 s1,0.45,1,1C22,10.05,21.55,10.5,21,10.5z" opacity=".3"/><path d="M21,6.5c-1.66,0-3,1.34-3,3c0,0.07,0,0.14,0.01,0.21l-2.03,0.68c-0.64-1.21-1.82-2.09-3.22-2.32V5.91 C14.04,5.57,15,4.4,15,3c0-1.66-1.34-3-3-3S9,1.34,9,3c0,1.4,0.96,2.57,2.25,2.91v2.16c-1.4,0.23-2.58,1.11-3.22,2.32L5.99,9.71 C6,9.64,6,9.57,6,9.5c0-1.66-1.34-3-3-3s-3,1.34-3,3s1.34,3,3,3c1.06,0,1.98-0.55,2.52-1.37l2.03,0.68 c-0.2,1.29,0.17,2.66,1.09,3.69l-1.41,1.77C6.85,17.09,6.44,17,6,17c-1.66,0-3,1.34-3,3s1.34,3,3,3s3-1.34,3-3 c0-0.68-0.22-1.3-0.6-1.8l1.41-1.77c1.36,0.76,3.02,0.75,4.37,0l1.41,1.77C15.22,18.7,15,19.32,15,20c0,1.66,1.34,3,3,3s3-1.34,3-3 s-1.34-3-3-3c-0.44,0-0.85,0.09-1.23,0.26l-1.41-1.77c0.93-1.04,1.29-2.4,1.09-3.69l2.03-0.68c0.53,0.82,1.46,1.37,2.52,1.37 c1.66,0,3-1.34,3-3S22.66,6.5,21,6.5z M3,10.5c-0.55,0-1-0.45-1-1c0-0.55,0.45-1,1-1s1,0.45,1,1C4,10.05,3.55,10.5,3,10.5z M6,21 c-0.55,0-1-0.45-1-1c0-0.55,0.45-1,1-1s1,0.45,1,1C7,20.55,6.55,21,6,21z M11,3c0-0.55,0.45-1,1-1s1,0.45,1,1c0,0.55-0.45,1-1,1 S11,3.55,11,3z M12,15c-1.38,0-2.5-1.12-2.5-2.5c0-1.38,1.12-2.5,2.5-2.5s2.5,1.12,2.5,2.5C14.5,13.88,13.38,15,12,15z M18,19 c0.55,0,1,0.45,1,1c0,0.55-0.45,1-1,1s-1-0.45-1-1C17,19.45,17.45,19,18,19z M21,10.5c-0.55,0-1-0.45-1-1c0-0.55,0.45-1,1-1 s1,0.45,1,1C22,10.05,21.55,10.5,21,10.5z"/></svg>
+                    </div>
+                </a>
+            </div>
+    </main>
+</body>
+
+</html>
--- prodoc/resources/views/documento/index.blade.php
+++ prodoc/resources/views/documento/index.blade.php
@@ -1,43 +1,66 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    
-    <title>Lista Documentos</title>
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-</head>
+<x-head />
+
 <body>
-<h1>Lista Documentos</h1>
-    <a href="{{Route('documento.create') }}" class="btn btn-lg btn-primary">Criar Documento</a>
-    <div class="row">
-    <table class="tabela table-striped">
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th>Descriçao</th>
-                <th>Status</th>
-            </tr>
-    <thead>
-        <tbody>
-            <!--Pega as informações do banco-->
-            @foreach($documentos as $doc)
-            <tr>
-                    <td>{{$doc->nome}}a</td>
-                    <td>{{$doc->descricao}}</td>
-                    <td>{{$doc->status}}</td>
-                    <td> 
-                        <a hred="#" class="btn btn-sm btn-info">Visualizar</a>
-                        <a hred="#" class="btn btn-sm btn-warning">Editar</a>
-                        <a hred="#" class="btn btn-sm btn-danger">Apagar</a>
-                        </td>
-            </tr>
-            @endforeach
+
+    <x-header />
+
+    <main class="container">
+
+        <h2 class="my-5">Lista de Documentos</h2>
+
+        <div class="br-table" ...>
+            <!-- Cabeçalho -->
+            <!-- Dados -->
+            <table class="tabela">
+                <caption>Lista de Documentos</caption>
+                <thead>
+                    <tr>
+                        <th scope="col">Nº do documento</th>
+                        <th scope="col">Tipo de documento</th>
+                        <th scope="col">Data de cadastro</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                    <thead>
+                    <tbody>
+                        <!--Pega as informações do banco-->
+                        @foreach($documentos as $doc)
+                        <tr>
+                            <!--<td>Nº do documento</td>-->
+                            <!--<td>Tipo de documento</td>-->
+                            <!--<td>Data de cadastro</td>-->
+                            <td>{{$doc->status}}</td>
+                            <td>
+
+                            <button class="br-button circle mt-3 mt-sm-0 ml-sm-3" type="button" aria-label="Visualizar"><i class="fas fa-eye" aria-hidden="true"></i>
+                            </button>
+
+                            <button class="br-button circle mt-3 mt-sm-0 ml-sm-3" type="button" aria-label="Editar"><i class="fas fa-edit" aria-hidden="true"></i>
+                                </button>
+
+                                <button class="br-button circle mt-3 mt-sm-0 ml-sm-3" type="button" aria-label="Encerrar"><i class="fas fa-trash" aria-hidden="true"></i>
+                                </button>
+    </main>
+
+                            </td>
+                        </tr>
+                        @endforeach
                     </tbody>
+            </table>
+            <!-- Rodapé -->
 
-</table>
+        </div>
 
-</div>
+        <div class="d-flex justify-content-end">
+        <button onclick="window.location='{{ url('documento/create'); }}'"
+            class="br-button primary mt-5" type="button">
+            Cadastrar documento
+        </button>
+        </div>
+
 </body>
+
 </html>

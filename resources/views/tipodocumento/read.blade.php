a7424128522669e1ba0e7387a045dc08ed690012
Luiz Carlos <63129289+lcgds@users.noreply.github.com>
2022-07-22 08:43:36 -0300

    Update read.blade.php

--- resources/views/tipodocumento/read.blade.php
+++ resources/views/tipodocumento/read.blade.php
@@ -36,7 +36,7 @@
             </ul>
         </div>
 
-        <p class="my-5 h4">Detalhes do Tipo de Documentos</p>
+        <h2 class="my-5">Detalhes do Tipo de Documento</h2>
 
         <div class="bg-gray-3 p-5">
 

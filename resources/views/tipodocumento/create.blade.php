77d707395c149d929fa4e2fa086559743de47dad
Luiz Carlos <63129289+lcgds@users.noreply.github.com>
2022-07-27 15:49:55 -0300

    Update create.blade.php

--- resources/views/tipodocumento/create.blade.php
+++ resources/views/tipodocumento/create.blade.php
@@ -22,11 +22,6 @@
 
                 <li class="crumb">
                     <i class="icon fas fa-chevron-right"></i>
-                    <a href="{{ Route('documento.index'); }}">Documentos</a>
-                </li>
-
-                <li class="crumb">
-                    <i class="icon fas fa-chevron-right"></i>
                     <a href="{{ Route('tipodocumento.index'); }}">Tipos de Documentos</a>
                 </li>
 
@@ -39,43 +34,42 @@
 
         <h2 class="my-5">Cadastro de Tipo de Documento</h2>
 
-        <form class="bg-gray-3 p-5 mb-5" method="POST" action="{{ route('tipodocumento.store') }}">
-
+        <form method="POST" action="{{ route('tipodocumento.store') }}">
             @csrf
+            <div class="bg-gray-3 p-5 mb-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 mb-4">
 
-            <div class="row">
-                <div class="col-lg-4 col-md-6 mb-4">
-
-                    <div class="br-input">
-                        <label for="input-tipoDocumento-nome" class="form-label">Título</label>
-                        <div class="input-group">
-                            <div class="input-icon"><i class="fas fa-file" aria-hidden="true"></i>
+                        <div class="br-input">
+                            <label for="input-tipoDocumento-nome" class="form-label">Título</label>
+                            <div class="input-group">
+                                <div class="input-icon"><i class="fas fa-file" aria-hidden="true"></i>
+                                </div>
+                                <input id="input-tipoDocumento-nome" type="text" class="form-control" name="nome"
+                                    placeholder="Ex.: Memorando" required autofocus></input>
                             </div>
-                            <input id="input-tipoDocumento-nome" type="text" class="form-control" name="nome"
-                                placeholder="Ex.: Memorando" required autofocus></input>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-lg-6 col-md-8 mb-4">
+                        <div class="br-textarea">
+                            <label for="descricao">Descrição</label>
+                            <textarea id="descricao" name="descricao"
+                                placeholder="Digite aqui a descrição do tipo de documento." rows="4" maxlength="300"
+                                required></textarea>
+                            <div class="text-base mt-1">
+                                <span class="limit">Limite máximo de <strong>300</strong> caracteres</span>
+                                <span class="current"></span>
+                            </div>
                         </div>
 
                     </div>
                 </div>
             </div>
-
-
-            <div class="row">
-                <div class="col-lg-6 col-md-8 mb-4">
-                    <div class="br-textarea">
-                        <label for="descricao">Descrição</label>
-                        <textarea id="descricao" name="descricao"
-                            placeholder="Digite aqui a descrição do tipo de documento." rows="4" maxlength="300"
-                            required></textarea>
-                        <div class="text-base mt-1">
-                            <span class="limit">Limite máximo de <strong>300</strong> caracteres</span>
-                            <span class="current"></span>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
             <div class="d-flex justify-content-end">
                 <x-btn-cancel />
                 <button type="submit" class="br-button primary">Cadastrar</button>

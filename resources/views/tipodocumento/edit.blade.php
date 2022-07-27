79a3a6dc490fddbfe1c5eef93199d16f8de0583a
Luiz Carlos <63129289+lcgds@users.noreply.github.com>
2022-07-27 15:52:34 -0300

    Update edit.blade.php

--- resources/views/tipodocumento/edit.blade.php
+++ resources/views/tipodocumento/edit.blade.php
@@ -33,44 +33,43 @@
 
         <h2 class="my-5">Dados do Tipo de Documento</h2>
 
-        <form class="bg-gray-3 p-5" method="POST" action="{{route ('tipodocumento.update', $tipodocumento->id) }}">
-
+        <form method="POST" action="{{route ('tipodocumento.update', $tipodocumento->id) }}">
             @csrf
+            <div class="bg-gray-3 p-5">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 mb-4">
 
-            <div class="row">
-                <div class="col-lg-4 col-md-6 mb-4">
-
-                    <div class="br-input">
-                        <label for="input-tipoDocumento-nome" class="form-label">Título</label>
-                        <div class="input-group">
-                            <div class="input-icon"><i class="fas fa-file" aria-hidden="true"></i>
+                        <div class="br-input">
+                            <label for="input-tipoDocumento-nome" class="form-label">Título</label>
+                            <div class="input-group">
+                                <div class="input-icon"><i class="fas fa-file" aria-hidden="true"></i>
+                                </div>
+                                <input id="input-tipoDocumento-nome" type="text" class="form-control" name="nome"
+                                    placeholder="Ex.: Memorando" value="{{$tipodocumento->nome}}"></input>
                             </div>
-                            <input id="input-tipoDocumento-nome" type="text" class="form-control" name="nome"
-                                placeholder="Ex.: Memorando" value="{{$tipodocumento->nome}}"></input>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-lg-6 col-md-8 mb-4">
+                        <div class="br-textarea">
+                            <label for="descricao">Descrição</label>
+                            <textarea id="descricao" name="descricao"
+                                placeholder="Digite aqui a descrição do tipo de documento." rows="4"
+                                maxlength="300">{{$tipodocumento->descricao}}</textarea>
+                            <div class="text-base mt-1">
+                                <span class="limit">Limite máximo de <strong>300</strong> caracteres</span>
+                                <span class="current"></span>
+                            </div>
                         </div>
 
                     </div>
                 </div>
             </div>
-
-
-            <div class="row">
-                <div class="col-lg-6 col-md-8 mb-4">
-                    <div class="br-textarea">
-                        <label for="descricao">Descrição</label>
-                        <textarea id="descricao" name="descricao"
-                            placeholder="Digite aqui a descrição do tipo de documento." rows="4"
-                            maxlength="300">{{$tipodocumento->descricao}}</textarea>
-                        <div class="text-base mt-1">
-                            <span class="limit">Limite máximo de <strong>300</strong> caracteres</span>
-                            <span class="current"></span>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
-
+            
             <div class="d-flex justify-content-end">
                 <x-btn-cancel />
                 <x-btn-update />

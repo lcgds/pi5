90c1b5c178ce14c0de8734945e56dbc0fab8e2ed
Luiz Carlos <63129289+lcgds@users.noreply.github.com>
2022-08-01 20:54:24 -0300

    Update tipodocumentoController.php

--- app/Http/Controllers/tipodocumentoController.php
+++ app/Http/Controllers/tipodocumentoController.php
@@ -45,11 +45,11 @@ class tipodocumentoController extends Controller {
     public
     function delete(tipodocumento $tipodocumento) {
         if ($tipodocumento->documentos->count() > 0) {
-            session()->flash('warning', $tipodocumento->documentos->count());
+            session()->flash('warning', 'O Tipo de Documento selecionado possui documentos ativos. Portanto, não é possível deletá-lo.');
             return redirect(route('tipodocumento.index'));
         } else {
             $tipodocumento->delete();
-            session()->flash('success', $tipodocumento->documentos->count());
+            session()->flash('success', 'O Tipo de Documento foi deletado.');
             return redirect(route('tipodocumento.index'));
         }
     }
